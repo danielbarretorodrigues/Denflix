a6329dcc6663c2ede219906fa2975e997ef26ded
Daniel Barreto Rodrigues <126365192+danielbarretorodrigues@users.noreply.github.com>
2026-08-25 21:48:47 -0300

    Update index.jsx

--- src/pages/Filme/index.jsx
+++ src/pages/Filme/index.jsx
@@ -75,7 +75,7 @@ function Filme(){
             <img src={`https://image.tmdb.org/t/p/original/${filme.backdrop_path}`} alt={filme.title} />
             <h3>Sinopse</h3>
             <span>{filme.overview}</span>
-            <strong>Avaliação: {filme.vote_average} / 10</strong>
+            <strong>Avaliação: {filme.vote_average.toFixed(1)} / 10</strong>
             
             <div className="area-buttons">
                 <button onClick={salvarFilme}>Salvar</button>
